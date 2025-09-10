0b8c3bd7f7b7d6ec811866ccfcd1948d9b8e66ea
ksh171115-coder <ksh171115@gmail.com>
2025-09-10 03:46:10 +0000

    타이틀 변경2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 hello suhyun")
+st.title("🎈 hello suhyun22222")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
